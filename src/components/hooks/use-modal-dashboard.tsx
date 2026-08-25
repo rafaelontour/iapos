6729188482003f530ccec5f38a3fb341cb37d4d5
Eric Lisboa Queiroz <ericxx101@gmail.com>
2026-08-25 12:54:23 -0300

    fix: adicionar linhas-pesquisa ao ModalType do dashboard

--- src/components/hooks/use-modal-dashboard.tsx
+++ src/components/hooks/use-modal-dashboard.tsx
@@ -1,7 +1,7 @@
 
 import { create } from "zustand";
 
-export type ModalType = "general" | "researcher" | "dados-pos-graduacao" | "graduate-program" | "master" | 'departamento' | 'peso-producao' | 'grupo-pesquisa' | 'indicadores' | 'baremas' | 'enviar-notificacoes' | 'informacoes' | 'minha-area' | 'home-dashboard' | 'minhas-producoes' | 'parametros-pesquisa' | 'sessao-pessoal' | 'construtor-pagina' | 'cargos-permissoes' | 'instituicoes' | 'programa' | 'departamentos' | 'datas' | 'tags' | 'areas'
+export type ModalType = "general" | "researcher" | "dados-pos-graduacao" | "graduate-program" | "master" | 'departamento' | 'peso-producao' | 'grupo-pesquisa' | 'indicadores' | 'baremas' | 'enviar-notificacoes' | 'informacoes' | 'minha-area' | 'home-dashboard' | 'minhas-producoes' | 'parametros-pesquisa' | 'sessao-pessoal' | 'construtor-pagina' | 'cargos-permissoes' | 'instituicoes' | 'programa' | 'departamentos' | 'datas' | 'tags' | 'areas' | 'linhas-pesquisa'
 
 
 interface ModalStore {
